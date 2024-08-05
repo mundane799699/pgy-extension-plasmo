997339ded6d3c1bd7821610a8259424959f86568
mundane <799699348@qq.com>
2024-08-05 20:36:43 +0800

    提交审核了

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,19 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "isActiveTab") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      sendResponse({ isActive: tabs[0].id === sender.tab.id })
-    })
-    return true // Will respond asynchronously.
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    const baseUrl = "https://mundane.ink"
+    // const baseUrl = "http://localhost:8088"
+    fetch(`${baseUrl}/mail/sendMail/send`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`)
+        }
+        return res.text()
+      })
+      .then((data) => {
+        console.log("安装通知已发送，服务器返回：", data)
+      })
+      .catch((err) => {
+        console.log("安装通知发送失败", err)
+      })
   }
 })
